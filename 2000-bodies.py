42619dea3ff9013c3bec30271ce753f4096ae042
luis.montes@ceiba.com.co <luis.montes@ceiba.com.co>
2022-08-17 11:57:05 -0500

    Add examples for simulate bodies gravity in a field

---
+++ 2000-bodies.py
@@ -0,0 +1,13 @@
+from gravity_simulation.gravity import GravityField
+from gravity_simulation.gravity import Body
+
+field = GravityField()
+
+field.generate_random(3500,mass=[100,500], r_x=[-40,40], r_y=[-40,40])
+
+field.add_body(Body(x0=22,y0=22,v_x=0,v_y=0,mass = 15000))
+
+field.add_body(Body(x0=-22,y0=-22,v_x=0,v_y=0,mass = 15000))
+
+field.run(1300,C = 0.01)
+field.save_animation(reduce_size_body=200,frames=200,title='galaxy')
